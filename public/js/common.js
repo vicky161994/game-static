c3e7c6962b3250d782bef85b636965d304d55874
Vicky Kumar <kumar.v005@gmail.com>
2021-11-29 23:21:18 +0530

    small changes

--- public/js/common.js
+++ public/js/common.js
@@ -3,7 +3,7 @@ $(document).scroll(function () {
   if (screenWidth > 991) {
     var y = $(this).scrollTop();
     console.log("scroll is: " + y);
-    if (y > 700 && y < 3700) {
+    if (y > 1200 && y < 4400) {
       $(".scroll-content").fadeIn();
     } else {
       $(".scroll-content").fadeOut();
